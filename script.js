7a892fbc61fe54bb559a3d58ef2e15f0f8764e85
Younes Derrar <younes.derrar@gmail.com>
2024-09-13 15:51:22 +0100

    Preuve du 20/20

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let message = "There is no war in Ba Sing Se";
-console.log(message.length);
+console.log(message.split(" ").length);
 
 let countries = ["China", "Japan", "South Korea", "Vietnam", "Malaysia"];
 let capital = ["Beijing", "Tokyo", "Seoul", "Hanoï", "Kuala Lumpur"];
